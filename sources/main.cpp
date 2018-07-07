c38e6f45837c04a62b16553470e13a331adc030f
saito <vqcuong96@gmail.com>
2018-07-08 01:21:56 +0700

    Update gitignore

---
+++ sources/main.cpp
@@ -0,0 +1,12 @@
+#include <opencv/cv.h>
+#include <iostream>
+using namespace std;
+using namespace cv;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    cout << "Hello World" << endl;
+    return 0;
+}
+
